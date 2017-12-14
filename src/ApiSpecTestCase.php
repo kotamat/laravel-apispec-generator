701491e69ece2ddaa605d64e49020df288afe4b4
kotamat <kota1861@gmail.com>
2017-12-14 13:30:53 +0900

    add flag

--- src/ApiSpecTestCase.php
+++ src/ApiSpecTestCase.php
@@ -6,17 +6,21 @@ use Illuminate\Foundation\Testing\TestCase;
 
 abstract class ApiSpecTestCase extends TestCase
 {
+    protected $isExportSpec = false;
+
     public function postJson($uri, array $data = [], array $headers = [])
     {
         $res = parent::postJson($uri, $data, $headers);
 
-        (new ApiSpecObject())->setMethod('POST')
-            ->setUri($uri)
-            ->setData($data)
-            ->setHeaders($headers)
-            ->setResponse($res)
-            ->setIsAuthenticated($this->isAuthenticated())
-            ->output();
+        if ($this->isExportSpec) {
+            (new ApiSpecObject())->setMethod('POST')
+                ->setUri($uri)
+                ->setData($data)
+                ->setHeaders($headers)
+                ->setResponse($res)
+                ->setIsAuthenticated($this->isAuthenticated())
+                ->output();
+        }
 
         return $res;
     }
@@ -24,12 +28,15 @@ abstract class ApiSpecTestCase extends TestCase
     public function getJson($uri, array $headers = [])
     {
         $res = parent::getJson($uri, $headers);
-        (new ApiSpecObject())->setMethod('GET')
-            ->setUri($uri)
-            ->setHeaders($headers)
-            ->setResponse($res)
-            ->setIsAuthenticated($this->isAuthenticated())
-            ->output();
+
+        if ($this->isExportSpec) {
+            (new ApiSpecObject())->setMethod('GET')
+                ->setUri($uri)
+                ->setHeaders($headers)
+                ->setResponse($res)
+                ->setIsAuthenticated($this->isAuthenticated())
+                ->output();
+        }
 
         return $res;
     }
@@ -38,13 +45,15 @@ abstract class ApiSpecTestCase extends TestCase
     {
         $res = parent::putJson($uri, $data, $headers);
 
-        (new ApiSpecObject())->setMethod('PUT')
-            ->setUri($uri)
-            ->setData($data)
-            ->setHeaders($headers)
-            ->setResponse($res)
-            ->setIsAuthenticated($this->isAuthenticated())
-            ->output();
+        if ($this->isExportSpec) {
+            (new ApiSpecObject())->setMethod('PUT')
+                ->setUri($uri)
+                ->setData($data)
+                ->setHeaders($headers)
+                ->setResponse($res)
+                ->setIsAuthenticated($this->isAuthenticated())
+                ->output();
+        }
 
         return $res;
     }
@@ -53,13 +62,15 @@ abstract class ApiSpecTestCase extends TestCase
     {
         $res = parent::delete($uri, $data, $headers);
 
-        (new ApiSpecObject())->setMethod('DELETE')
-            ->setUri($uri)
-            ->setData($data)
-            ->setHeaders($headers)
-            ->setResponse($res)
-            ->setIsAuthenticated($this->isAuthenticated())
-            ->output();
+        if ($this->isExportSpec) {
+            (new ApiSpecObject())->setMethod('DELETE')
+                ->setUri($uri)
+                ->setData($data)
+                ->setHeaders($headers)
+                ->setResponse($res)
+                ->setIsAuthenticated($this->isAuthenticated())
+                ->output();
+        }
 
         return $res;
     }
