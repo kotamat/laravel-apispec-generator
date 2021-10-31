ccf18aa7d210d62d8f8ce28d2c0508356bc9be97
kotamat <kota1861@gmail.com>
2021-10-31 15:31:57 +0900

    add service provider and auto discovery

--- src/ApiSpecServiceProvider.php
+++ src/ApiSpecServiceProvider.php
@@ -1,17 +1,31 @@
 <?php
+declare(strict_types=1);
 
 namespace ApiSpec;
 
 
 use ApiSpec\Builders\BuilderInterface;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class ApiSpecServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(BuilderInterface::class,function($app,$name,array $config){
-            return new $config['builder'];
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/apispec.php', 'apispec'
+        );
+        $this->app->bind(BuilderInterface::class, function (Application $app) {
+            $builderClass = $app->make('config')->get('apispec.builder');
+
+            return $app->make($builderClass);
         });
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/apispec.php' => config_path('apispec.php'),
+        ]);
     }
 }
